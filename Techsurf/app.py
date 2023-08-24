ade0ae4057c8ea48c304bf86aacf08ae6fd8473a
LAKSHMANA-VINAY <109097305+LAKSHMANA-VINAY@users.noreply.github.com>
2023-08-24 15:48:05 +0530

    Update app.py

--- Techsurf/app.py
+++ Techsurf/app.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template, request
+from transformers import pipeline
 from transformers import TFGPT2LMHeadModel, GPT2Tokenizer
 import tensorflow as tf
 
 app = Flask(__name__)
 
+summerizer=pipeline('summarization')
 tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 model = TFGPT2LMHeadModel.from_pretrained("gpt2", pad_token_id=tokenizer.eos_token_id)
+
+
+def summerize_text(inp):
+    summary=summerizer(inp,max_length=100,min_length=15,do_sample=False)[0]['summary_text']
+    return summary
 
 def generate_text(inp):
     input_ids = tokenizer.encode(inp, return_tensors="tf")
@@ -18,7 +25,9 @@ def index():
     output_text = ""
     if request.method == "POST":
         input_text = request.form.get("input_text")
-        output_text = generate_text(input_text)
+        summary=summerize_text(input_text)
+        full_content = input_text+"\n"+summary
+        output_text = generate_text(full_content)
     return render_template("index.html", output_text=output_text)
 
 if __name__ == "__main__":
