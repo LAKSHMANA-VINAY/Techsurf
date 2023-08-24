119c7b744e4636fe2b92bb5a26208392b43a4dbe
LAKSHMANA-VINAY <109097305+LAKSHMANA-VINAY@users.noreply.github.com>
2023-08-24 15:25:52 +0530

    Add files via upload

---
+++ Techsurf/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+from transformers import TFGPT2LMHeadModel, GPT2Tokenizer
+import tensorflow as tf
+
+app = Flask(__name__)
+
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+model = TFGPT2LMHeadModel.from_pretrained("gpt2", pad_token_id=tokenizer.eos_token_id)
+
+def generate_text(inp):
+    input_ids = tokenizer.encode(inp, return_tensors="tf")
+    beam_output = model.generate(input_ids, max_length=500, num_beams=3, do_sample=True, no_repeat_ngram_size=5, top_k=50, temperature=0.8, early_stopping=True)
+    output = tokenizer.decode(beam_output[0], skip_special_tokens=True, clean_up_tokenization_spaces=True)
+    return ".".join(output.split(".")[:-1]) + "."
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    output_text = ""
+    if request.method == "POST":
+        input_text = request.form.get("input_text")
+        output_text = generate_text(input_text)
+    return render_template("index.html", output_text=output_text)
+
+if __name__ == "__main__":
+    app.run(debug=True)
